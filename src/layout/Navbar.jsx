970d6b34fa61d83a6a31d706b06b0dd6fbe2d766
keylane1355 <keylaneaaraujo@gmail.com>
2026-05-07 17:47:22 -0300

    Criação da estrutura da Navbar e mapeamento de links
    Created the navLinks array and implement the map function to render navigation items.Added tailwind classes for fixed positioning and flex layout.

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,4 +1,30 @@
-export const Navbar =() => {
-  return <header></header>;
 
-}
+
+const navLinks = [
+  {href: "#about", label: "Sobre"},
+  {href: "#projects", label: "Projetos"},
+  {href: "#experience", label: "Experiência"},
+  {href: "#testimonials", label: "Testimonials"},
+]
+export const Navbar =() => {
+  return ( 
+  <header className="fixed top-0 left-0 right-0 bg-transparent py-5">
+    <nav className="container mx-auto px-6 flex items-center justify-between">
+      <a href="#" className="text-xl font-bold tracking-light">
+        KS<span>.</span>
+        </a>
+
+        {/* Desktop Nav */}
+        <div>
+          <div>
+            {navLinks.map((link,index)=> (
+              <a href={link.href} key={index}>
+                {link.label} 
+                </a>
+            ))}
+          </div>
+        </div> 
+    </nav>
+  </header>
+  );
+};
