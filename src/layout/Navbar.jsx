fd5d6cc79ebacadb5e00a8826238847b50339c32
keylane1355 <keylaneaaraujo@gmail.com>
2026-05-13 19:55:22 -0300

    Create Button
    Criação do desing do botão

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,3 +1,4 @@
+import { Button } from "../components/Button"; 
 
 
 const navLinks = [
@@ -10,20 +11,29 @@ export const Navbar =() => {
   return ( 
   <header className="fixed top-0 left-0 right-0 bg-transparent py-5">
     <nav className="container mx-auto px-6 flex items-center justify-between">
-      <a href="#" className="text-xl font-bold tracking-light">
-        KS<span>.</span>
+      <a href="#" className="text-xl font-bold tracking-tight hover:text-primary">
+        KS<span className="text-primary">.</span>
         </a>
 
         {/* Desktop Nav */}
-        <div>
-          <div>
+        <div className="hidden md:flex items-center gap-1">
+          <div className="glass rounded-full px-2 py-1 flex items-center gap-1">
             {navLinks.map((link,index)=> (
-              <a href={link.href} key={index}>
+              <a href={link.href} 
+              key={index} 
+              className="px-4 py-2 text-muted-foreground hover:text-foreground rounded-full hover:bg-surface "
+              >
                 {link.label} 
                 </a>
             ))}
           </div>
         </div> 
+
+        {/* Cta Botão  */}
+        <div>
+          <Button size="sm"> Contato </Button>
+        
+        </div>
     </nav>
   </header>
   );
